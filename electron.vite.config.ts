7ea1aa3b0b5ec97c2adfb4a44a42c9711525b1ef
kangod <kangod.yan@gmail.com>
2024-03-14 14:16:21 +0800

    chore: vite 配置继承 tsconfig 别名，并定义 define 消除 util.inherits 的报错

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path"
+import { defineConfig, externalizeDepsPlugin } from "electron-vite"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    main: {
+        plugins: [externalizeDepsPlugin()],
     },
-    plugins: [react()]
-  }
+    preload: {
+        plugins: [externalizeDepsPlugin()],
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                "@renderer": resolve("src/renderer/src"),
+            },
+        },
+        define: {
+            "process.env": {},
+        },
+        plugins: [react(), tsconfigPaths()],
+    },
 })

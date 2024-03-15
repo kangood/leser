fb7cee1378440c1861cf8dcc7b574d923fd5403b
kangod <kangod.yan@gmail.com>
2024-03-15 19:24:35 +0800

    chore: 在 vite 中增加 rollup-plugin-copy 插件的使用，把资源文件在构建后复制到输出目录

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,10 +2,19 @@ import { resolve } from "path"
 import { defineConfig, externalizeDepsPlugin } from "electron-vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import copy from "rollup-plugin-copy"
 
 export default defineConfig({
     main: {
-        plugins: [externalizeDepsPlugin()],
+        plugins: [
+            externalizeDepsPlugin(),
+            // 在构建之后复制文件到输出目录
+            copy({
+                // 用 closeBundle 替代默认的 buildEnd
+                hook: "closeBundle",
+                targets: [{ src: "resources/font*", dest: "out/main" }],
+            }),
+        ],
     },
     preload: {
         plugins: [externalizeDepsPlugin()],

d3dd24725aa0635b56f9f20df59254a7f47f0d9d
kangod <kangod.yan@gmail.com>
2024-04-03 15:40:21 +0800

    replenish: 修复内容过滤的默认值问题

--- src/renderer/src/components/utils/content-filter.tsx
+++ src/renderer/src/components/utils/content-filter.tsx
@@ -4,44 +4,37 @@ import intl from 'react-intl-universal';
 import { FilterType } from '@renderer/scripts/models/feed';
 
 export const ContentFilter = ({ filter, switchFilter }) => {
-    console.log('oldF', filter);
-    const [activeFilter, setActiveFilter] = useState((filter | ((FilterType.Default | FilterType.CaseInsensitive) & FilterType.Toggles)));
-
-    const handleClick = (filterType) => {
-        switchFilter(filterType);
-        setActiveFilter((filterType | (filter & FilterType.Toggles)));
-    };
-
-    const isActiveFilter = (filterType) => {
-        return activeFilter === (filterType | (filter & FilterType.Toggles));
+    // 判断当前是否被选中
+    const checked = (filterType: FilterType) => {
+        return (filter & ~FilterType.Toggles) == filterType;
     }
 
     return (
         <div className="content-filter">
             <span className="seperator"></span>
             <a
-                className={`btn undragging ${isActiveFilter(FilterType.StarredOnly) ? 'selected' : ''}`}
-                onClick={() => handleClick(FilterType.StarredOnly)}
+                className={`btn undragging ${checked(FilterType.StarredOnly) ? 'selected' : ''}`}
+                onClick={() => switchFilter(FilterType.StarredOnly)}
                 title={intl.get("context.starredOnly")}
             >
                 <Icon iconName="FavoriteStarFill" />
-                {isActiveFilter(FilterType.StarredOnly) && <span>星标</span>}
+                {checked(FilterType.StarredOnly) && <span>星标</span>}
             </a>
             <a
-                className={`btn undragging ${isActiveFilter(FilterType.UnreadOnly) ? 'selected' : ''}`}
-                onClick={() => handleClick(FilterType.UnreadOnly)}
+                className={`btn undragging ${checked(FilterType.UnreadOnly) ? 'selected' : ''}`}
+                onClick={() => switchFilter(FilterType.UnreadOnly)}
                 title={intl.get("context.unreadOnly")}
             >
                 <Icon iconName="RadioBtnOn" />
-                {isActiveFilter(FilterType.UnreadOnly) && <span>未读</span>}
+                {checked(FilterType.UnreadOnly) && <span>未读</span>}
             </a>
             <a
-                className={`btn undragging ${isActiveFilter(FilterType.Default) ? 'selected' : ''}`}
-                onClick={() => handleClick(FilterType.Default)}
+                className={`btn undragging ${checked(FilterType.Default) ? 'selected' : ''}`}
+                onClick={() => switchFilter(FilterType.Default)}
                 title={intl.get("allArticles")}
             >
                 <Icon iconName="ClearFilter" />
-                {isActiveFilter(FilterType.Default) && <span>全部</span>}
+                {checked(FilterType.Default) && <span>全部</span>}
             </a>
         </div>
     );

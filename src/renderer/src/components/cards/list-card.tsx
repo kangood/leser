9a16ae72db4f6491299e416728576b21e1fbd083
kangod <kangod.yan@gmail.com>
2024-04-09 15:48:38 +0800

    fix: 解决 wechat2rss 的 bug，网站地址返回不对的时候，封面图的 URL 某些情况下可能会多了个 `https//`

--- src/renderer/src/components/cards/list-card.tsx
+++ src/renderer/src/components/cards/list-card.tsx
@@ -15,41 +15,48 @@ const className = (props: CardProps) => {
     return cn.join(" ")
 }
 
-const ListCard: React.FunctionComponent<CardProps> = props => (
-    <div
-        className={className(props)}
-        {...bindCardEventsToProps(props)}
-        data-iid={props.item._id}
-        data-is-focusable
-    >
-        <div className="data">
-            <CardInfo source={props.source} item={props.item} />
-        </div>
-        <div className="bottom">
-            <div className="content" style={{width: (props.item.thumb && props.viewConfigs & ViewConfigs.ShowCover) ? 'calc(100% - 90px)' : '100%'}}>
-                <h3 className="title">
-                    <Highlights
-                        text={props.item.title}
-                        filter={props.filter}
-                        title
-                    />
-                </h3>
-                {Boolean(props.viewConfigs & ViewConfigs.ShowSnippet) && (
-                    <p className="snippet">
-                        <Highlights
-                            text={props.item.snippet}
-                            filter={props.filter}
-                        />
-                    </p>
-                )}
+const ListCard: React.FunctionComponent<CardProps> = props => {
+    // 解决 wechat2rss 的 bug, 网站地址返回不对的时候，封面图的 URL 某些情况下可能会多了个 `https//`
+    let thumbUrl = props.item.thumb;
+    if (thumbUrl.includes('https://https//')) {
+        thumbUrl = thumbUrl.replace('https://https//', 'https://');
+    }
+    return (
+        <div
+            className={className(props)}
+            {...bindCardEventsToProps(props)}
+            data-iid={props.item._id}
+            data-is-focusable
+        >
+            <div className="data">
+                <CardInfo source={props.source} item={props.item} />
             </div>
-            {props.item.thumb && props.viewConfigs & ViewConfigs.ShowCover ? (
-                <div className="cover">
-                    <img src={props.item.thumb} />
+            <div className="bottom">
+                <div className="content" style={{width: (thumbUrl && props.viewConfigs & ViewConfigs.ShowCover) ? 'calc(100% - 90px)' : '100%'}}>
+                    <h3 className="title">
+                        <Highlights
+                            text={props.item.title}
+                            filter={props.filter}
+                            title
+                        />
+                    </h3>
+                    {Boolean(props.viewConfigs & ViewConfigs.ShowSnippet) && (
+                        <p className="snippet">
+                            <Highlights
+                                text={props.item.snippet}
+                                filter={props.filter}
+                            />
+                        </p>
+                    )}
                 </div>
-            ) : null}
+                {thumbUrl && props.viewConfigs & ViewConfigs.ShowCover ? (
+                    <div className="cover">
+                        <img src={thumbUrl} />
+                    </div>
+                ) : null}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default ListCard
